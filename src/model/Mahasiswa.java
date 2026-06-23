61961b90ff5a790b1016d950eb3e5091fd6acf88
vicky <laptopvicky9@gmail.com>
2026-06-23 15:29:18 +0700

    model mahasiswa

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String prodi;
+    private int semester;
+
+    public Mahasiswa(String nim, String nama, String prodi, int semester) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.semester = semester;
+    }
+
+
+
+
+    // getter & setter
 }
